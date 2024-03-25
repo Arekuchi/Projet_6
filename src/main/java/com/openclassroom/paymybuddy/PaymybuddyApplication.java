6c8260621de179505bf60e89609359be2b0c4848
Alexis Berthelot <berthelota2@gmail.com>
2024-03-25 12:07:13 +0100

    maintenance

--- src/main/java/com/openclassroom/paymybuddy/PaymybuddyApplication.java
+++ src/main/java/com/openclassroom/paymybuddy/PaymybuddyApplication.java
@@ -3,6 +3,8 @@ package com.openclassroom.paymybuddy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//maintenance
+
 @SpringBootApplication
 public class PaymybuddyApplication {
 
